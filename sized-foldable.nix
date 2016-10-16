921b271130fd232568d9a5a3df78a10f9fe0b407
knupfer <fknupfer@gmail.com>
2016-10-16 23:45:12 +0200

    bump version

--- sized-foldable.nix
+++ sized-foldable.nix
@@ -1,7 +1,7 @@
 { mkDerivation, base, stdenv }:
 mkDerivation {
   pname = "sized-foldable";
-  version = "0.1.0.0";
+  version = "0.1.1.0";
   src = ./.;
   libraryHaskellDepends = [ base ];
   description = "Annotes foldables with their minimum and maximum size without performance impact";
